0ff91012985282f115c8795ce762fd8cb3126418
Rahul RV <rahul.rv87@gmail.com>
2015-08-02 14:49:21 -0400

    Adding item click listener to list row

---
+++ app/src/main/java/com/rahulrvr/comicme/interfaces/IListItemClickListener.java
@@ -0,0 +1,9 @@
+package com.rahulrvr.comicme.interfaces;
+
+/**
+ * Copyright (c) 2015 Elsevier, Inc. All rights reserved.
+ */
+public interface IListItemClickListener {
+
+    void onItemClicked(String tag);
+}
